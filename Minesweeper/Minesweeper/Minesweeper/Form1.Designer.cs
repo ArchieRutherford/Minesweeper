0749b935df8d469146986d6c4e3c51659dc1d1e4
ArchieRutherford <33519215+ArchieRutherford@users.noreply.github.com>
2019-02-05 15:23:48 +0000

    Game finshed and working
    Probably

--- Minesweeper/Minesweeper/Minesweeper/Form1.Designer.cs
+++ Minesweeper/Minesweeper/Minesweeper/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Minesweeper
 {
-    partial class game
+    partial class frmGame
     {
         /// <summary>
         /// Required designer variable.
@@ -30,11 +30,14 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.changeDifficultyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,7 +49,7 @@
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(484, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -59,6 +62,20 @@
             this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
             this.gameToolStripMenuItem.Text = "Game";
             // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
             // optionsToolStripMenuItem
             // 
             this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -67,38 +84,51 @@
             this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
             this.optionsToolStripMenuItem.Text = "Options";
             // 
+            // changeDifficultyToolStripMenuItem
+            // 
+            this.changeDifficultyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem,
+            this.mediumToolStripMenuItem,
+            this.hardToolStripMenuItem});
+            this.changeDifficultyToolStripMenuItem.Name = "changeDifficultyToolStripMenuItem";
+            this.changeDifficultyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.changeDifficultyToolStripMenuItem.Text = "Change Difficulty";
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.easyToolStripMenuItem.Text = "Easy";
+            this.easyToolStripMenuItem.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            this.mediumToolStripMenuItem.Click += new System.EventHandler(this.mediumToolStripMenuItem_Click);
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hardToolStripMenuItem.Text = "Hard";
+            this.hardToolStripMenuItem.Click += new System.EventHandler(this.hardToolStripMenuItem_Click);
+            // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
             this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
             this.aboutToolStripMenuItem.Text = "About";
             // 
-            // newGameToolStripMenuItem
-            // 
-            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
-            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.newGameToolStripMenuItem.Text = "New Game";
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            // 
-            // changeDifficultyToolStripMenuItem
-            // 
-            this.changeDifficultyToolStripMenuItem.Name = "changeDifficultyToolStripMenuItem";
-            this.changeDifficultyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.changeDifficultyToolStripMenuItem.Text = "Change Difficulty";
-            // 
-            // game
+            // frmGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 461);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "game";
+            this.Name = "frmGame";
             this.RightToLeftLayout = true;
             this.Text = "Minesweeper";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -118,6 +148,9 @@
         private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem changeDifficultyToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
     }
 }
 
--- Minesweeper/Minesweeper/Minesweeper/Form1.cs
+++ Minesweeper/Minesweeper/Minesweeper/Form1.cs
@@ -9,69 +9,110 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Minesweeper
-{   //   EXTEND PREVIOUS TO CREATE A 2-D ARRAY OF BUTTONS (each with number on)
-    public partial class game : Form
+{ 
+    public partial class frmGame : Form
     {
-        static int sizeX = 6, sizeY = 6;
-        Button[,] btn = new Button[sizeX, sizeY];       // Create 2D array of buttons
-        int[,] board = new int[sizeX, sizeY];
+        int sizeX = 9, sizeY = 9, bombCount = 2;
+        Button[,] btn = new Button[30, 16];
+        int[,] board = new int[30, 16];
+        bool firstPress, gameEnded = false;
 
-        public game()
+        public frmGame()
         {
+
             InitializeComponent();
-            for (int x = 0; x < sizeX; x++)         // Loop for x
+            Size = new System.Drawing.Size(18 + (35 * sizeX), 70 + (35 * sizeY));
+
+            for (int x = 0; x < 30; x++)         // Loop for x
             {
-                for (int y = 0; y < sizeY; y++)     // Loop for y
+                for (int y = 0; y < 16; y++)     // Loop for y
                 {
-                    board[x,y] = 0;
                     btn[x, y] = new Button();
-                    btn[x, y].SetBounds(55 * x, 30 + (55 * y), 45, 45);
-                    btn[x, y].BackColor = Color.PowderBlue;
+                }
+            }
+            startGame();
+        }
+
+        void btnEvent_Click(object sender, EventArgs e)
+        {
+            int x = ((((Button)sender).Location.X) / 35);
+            int y = ((((Button)sender).Location.Y) / 35);
+
+            if (bombCheck(x, y) > 0 && !firstPress)
+                newGame();
+            else
+            {
+                firstPress = true;
+                openBox(x, y);
+            }
+        }
+
+        void startGame()
+        {
+            firstPress = false;
+            gameEnded = false;
+
+            for (int x = 0; x < sizeX; x++)         
+            {
+                for (int y = 0; y < sizeY; y++)     
+                {
+                    board[x, y] = 0;
+                    btn[x, y].Enabled = true;
+                    btn[x, y].SetBounds(35 * x, 30 + (35 * y), 35, 35);
+                    btn[x, y].BackColor = Color.BlueViolet;
+                    btn[x, y].Text = "";
                     btn[x, y].Click += new EventHandler(this.btnEvent_Click);
                     Controls.Add(btn[x, y]);
                 }
             }
-            placeBombs(6);
+            placeBombs(bombCount);
         }
-        void btnEvent_Click(object sender, EventArgs e)
-        {
-            int x = ((((Button)sender).Location.X) / 55);
-            int y = ((((Button)sender).Location.Y) / 55);
 
-            openBox(x, y, sender);
+        void newGame()
+        {
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    Controls.Remove(btn[x, y]);
+                }
+            }
+
+            startGame();
         }
-       
-        void openBox(int x, int y, object sender)
+
+        void openBox(int x, int y)
         {
             int noOfBombs = 0;
 
             if (board[x, y] < 2)
             {
                 board[x, y] += 2;
-                ((Button)sender).BackColor = Color.Aquamarine;
+                btn[x, y].BackColor = Color.LightBlue;
 
                 if (board[x, y] % 2 == 1)
                 {
-                    //Trigger game over
-                    ((Button)sender).BackColor = Color.Red;
+                    endGame(1);
                 }
                 else
                 {
                     noOfBombs = bombCheck(x, y);
-                    ((Button)sender).Text = Convert.ToString(noOfBombs);
-
+                    
                     if (noOfBombs == 0)
                     {
-                        openSurroundingBoxes(x, y, sender);
+                        openSurroundingBoxes(x, y);
                     }
+                    else
+                    {
 
-                    // TODO open all open surrounding boxes
-
+                        btn[x, y].Text = Convert.ToString(noOfBombs);
+                    }
                 }
             }
+            checkWin();
         }
 
-        void openSurroundingBoxes(int x, int y, object sender)
+        void openSurroundingBoxes(int x, int y)
         {
             for (int i = -1; i < 2; i++)
             {
@@ -81,7 +122,7 @@ namespace Minesweeper
                     {
                         if (board[x + i, y + j] < 2)
                         {
-                            openBox(x + i, y + j, sender);
+                            openBox(x + i, y + j);
                         }
                     }
                 }
@@ -127,11 +168,90 @@ namespace Minesweeper
             return 0;
         }
 
+        void checkWin()
+        {
+            int unopenedBoxes = 0;
+
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    if (board[x, y] < 2 && gameEnded == false)
+                    {
+                        unopenedBoxes++;
+                    }
+                }
+            }
+            
+            if (unopenedBoxes == bombCount)
+                endGame(0);
+        }
+
+        void endGame(int state)
+        {
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    if (board[x, y] % 2 == 1)
+                    {
+                        btn[x, y].BackColor = Color.Red;
+                    }
+
+                    btn[x, y].Enabled = false;
+                }
+            }
+
+            gameEnded = true;
+
+            if (state == 0)
+                MessageBox.Show("You have won the game!", "Congratulations");
+            else if (state == 1)
+                MessageBox.Show("You have hit a bomb!", "Unlucky");
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newGame();
+        }
+
+        private void easyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sizeX = 9;
+            sizeY = 9;
+            bombCount = 10;
+
+            Size = new System.Drawing.Size(18 + (35 * sizeX), 70 + (35 * sizeY));
+            newGame();
+        }
+
+        private void mediumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sizeX = 16;
+            sizeY = 16;
+            bombCount = 40;
+
+            Size = new System.Drawing.Size(18 + (35 * sizeX), 70 + (35 * sizeY));
+            newGame();
+        }
+
+        private void hardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sizeX = 30;
+            sizeY = 16;
+            bombCount = 99;
+
+            Size = new System.Drawing.Size(18 + (35 * sizeX), 70 + (35 * sizeY));
+            newGame();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {  //REQUIRED
         }
     }
 }
-
-
